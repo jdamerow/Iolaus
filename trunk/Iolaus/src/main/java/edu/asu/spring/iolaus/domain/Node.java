369af9d483612f45ebbec1c109ead7fc46f77da6
dlohith <dlohith@f4818639-45f7-4aff-89be-ea69564cad2b>
2013-10-03 00:16:03 +0000

    Generic Node

--- trunk/Iolaus/src/main/java/edu/asu/spring/iolaus/domain/Node.java
+++ trunk/Iolaus/src/main/java/edu/asu/spring/iolaus/domain/Node.java
@@ -3,19 +3,30 @@ package edu.asu.spring.iolaus.domain;
 import java.util.Set;
 
 import org.springframework.data.neo4j.annotation.GraphId;
+import org.springframework.data.neo4j.annotation.GraphProperty;
 import org.springframework.data.neo4j.annotation.Indexed;
 import org.springframework.data.neo4j.annotation.NodeEntity;
 import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.annotation.RelatedTo;
 import org.springframework.data.neo4j.annotation.RelatedToVia;
+import org.springframework.data.neo4j.fieldaccess.DynamicProperties;
+import org.springframework.data.neo4j.fieldaccess.DynamicPropertiesContainer;
 
 @NodeEntity
 public class Node {
 	
-  @GraphId Long id;
+  @GraphId 
+  Long id;
 
-  @Indexed(unique=true)
+  @Indexed(unique=false)
   private String type;
+
+  @Indexed(unique=false)
+  private String label;
   
+  private String dataset;
+  
+  @GraphProperty
+  DynamicProperties properties;
   
 }
