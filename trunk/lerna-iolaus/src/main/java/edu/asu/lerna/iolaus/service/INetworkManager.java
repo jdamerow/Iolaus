7200a2dc5f6e305f9f51e988bd5f5687ae904b36
dlohith <dlohith@f4818639-45f7-4aff-89be-ea69564cad2b>
2013-10-09 23:08:00 +0000

    adding repository

--- trunk/lerna-iolaus/src/main/java/edu/asu/lerna/iolaus/service/INetworkManager.java
+++ trunk/lerna-iolaus/src/main/java/edu/asu/lerna/iolaus/service/INetworkManager.java
@@ -7,5 +7,9 @@ public interface INetworkManager {
 
 	void saveRelation(Relation r);
 	void saveNode(Node n);
+	Node checkGetLocation(String city, String state);
+	Node checkGetSeries(String name);
+	Node checkGetInstitute(String name);
+	Node checkGetPerson(String firstName, String lastName);
 
 }
