107bcbb438dad184958e988ba83243e6209710e3
dlohith <dlohith@f4818639-45f7-4aff-89be-ea69564cad2b>
2013-10-25 21:38:40 +0000

    Parsing through query object

--- lerna-iolaus/src/main/java/edu/asu/lerna/iolaus/service/IXMLtoCypherConverter.java
+++ lerna-iolaus/src/main/java/edu/asu/lerna/iolaus/service/IXMLtoCypherConverter.java
@@ -1,5 +1,11 @@
 package edu.asu.lerna.iolaus.service;
 
+import javax.xml.bind.JAXBElement;
+
+import edu.asu.lerna.iolaus.domain.queryobject.Query;
+
 public interface IXMLtoCypherConverter {
+
+	void parseQuery(JAXBElement<Query> response);
 
 }
--- lerna-iolaus/src/main/java/edu/asu/lerna/iolaus/service/impl/XMLtoCypherConverter.java
+++ lerna-iolaus/src/main/java/edu/asu/lerna/iolaus/service/impl/XMLtoCypherConverter.java
@@ -1,10 +1,26 @@
 package edu.asu.lerna.iolaus.service.impl;
 
+import javax.xml.bind.JAXBElement;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import edu.asu.lerna.iolaus.domain.queryobject.Query;
 import edu.asu.lerna.iolaus.service.IXMLtoCypherConverter;
 
 @Service
 public class XMLtoCypherConverter implements IXMLtoCypherConverter{
-
+	
+	private static final Logger logger = LoggerFactory
+			.getLogger(XMLtoCypherConverter.class);
+	
+	@Override
+	public void parseQuery(JAXBElement<Query> response){
+		
+		Query q = response.getValue();
+		logger.info("Database ID : "+q.getDatabaseId(q.getDatabase()));
+		logger.info("Dataset Name : "+q.getDatasetName(q.getDataset()));
+		q.getNodeDetails(q.getNode());
+	}
 }
