d999ca3f39ffe4cbe484eb02efb55e59928fa8bf
dlohith <dlohith@f4818639-45f7-4aff-89be-ea69564cad2b>
2013-11-05 16:24:22 +0000

    Adding Interface

---
+++ lerna-iolaus/src/main/java/edu/asu/lerna/iolaus/domain/queryobject/IDatabase.java
@@ -0,0 +1,34 @@
+package edu.asu.lerna.iolaus.domain.queryobject;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSeeAlso;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import edu.asu.lerna.iolaus.domain.queryobject.impl.Database;
+import edu.asu.lerna.iolaus.domain.queryobject.impl.Dataset;
+
+@XmlJavaTypeAdapter(Database.Adapter.class)
+@XmlSeeAlso(Database.class)
+public interface IDatabase {
+
+	/**
+	 * Gets the value of the id property.
+	 * 
+	 * @return
+	 *     possible object is
+	 *     {@link String }
+	 *     
+	 */
+	public abstract String getId();
+
+	/**
+	 * Sets the value of the id property.
+	 * 
+	 * @param value
+	 *     allowed object is
+	 *     {@link String }
+	 *     
+	 */
+	public abstract void setId(String value);
+
+}
