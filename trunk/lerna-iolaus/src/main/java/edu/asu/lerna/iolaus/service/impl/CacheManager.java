b9ff779aa0b4f3f957ad87ac0058a3f28e1bdd4b
zambr0 <zambr0@f4818639-45f7-4aff-89be-ea69564cad2b>
2013-11-21 19:01:41 +0000

    Completed json parsing from neo4j

--- trunk/lerna-iolaus/src/main/java/edu/asu/lerna/iolaus/service/impl/CacheManager.java
+++ trunk/lerna-iolaus/src/main/java/edu/asu/lerna/iolaus/service/impl/CacheManager.java
@@ -13,6 +13,8 @@ import edu.asu.lerna.iolaus.configuration.neo4j.iml.Neo4jConfFile;
 import edu.asu.lerna.iolaus.configuration.neo4j.iml.Neo4jRegistry;
 import edu.asu.lerna.iolaus.domain.json.IJsonNode;
 import edu.asu.lerna.iolaus.domain.json.IJsonRelation;
+import edu.asu.lerna.iolaus.domain.json.impl.JsonNode;
+import edu.asu.lerna.iolaus.domain.json.impl.JsonRelation;
 import edu.asu.lerna.iolaus.service.ICacheManager;
 import edu.asu.lerna.iolaus.service.IRepositoryHandler;
 
@@ -20,7 +22,7 @@ import edu.asu.lerna.iolaus.service.IRepositoryHandler;
 public class CacheManager implements ICacheManager {
 
 	//TODO: Change this implementation to use autowiring
-//	private List<IRepositoryHandler> repoHanlders;
+	//	private List<IRepositoryHandler> repoHanlders;
 	@Autowired
 	private IRepositoryHandler repoHandler;
 	@Autowired
@@ -28,71 +30,73 @@ public class CacheManager implements ICacheManager {
 
 	public CacheManager()
 	{
-		
-//		repoHanlders = new ArrayList<IRepositoryHandler>();
+
+		//		repoHanlders = new ArrayList<IRepositoryHandler>();
 	}
-	
+
 	@Override
 	public void executeQuery(String json)
 	{
 		//TODO: Iterate through each repository and for each repository fetch the result.
 		System.out.println("\n\n");
+		json = "{\"query\":\"Start source=node:node_auto_index(type={param1}) match source-[r]->(target) Where (( source.dataset={param3} )) return source, r, target\", \"params\":{\"param1\":\"Person\",\"param3\":\"mblcourses\"}}";
+		List<List> listOfNodesAndRelations = repoHandler.executeQuery(json);
+		printList(listOfNodesAndRelations);
+
 		Iterator<Neo4jConfFile> fileIterator = neo4jInstances.getfileList().iterator();
 		while(fileIterator.hasNext())
 		{
 
-		json = fileIterator.next().getPath();
-		HashMap<String, List> listOfNodesAndRelations = repoHandler.executeQuery(json);
-		printList(listOfNodesAndRelations);
-		
-		json = "http://localhost:7474/db/data/node/4237/relationships/all";
-		listOfNodesAndRelations = repoHandler.executeQuery(json);
-		listOfNodesAndRelations = repoHandler.executeQuery(json);
-		printList(listOfNodesAndRelations);
-		
-		//TODO: Add the result to the resultList	
+			//TODO: Add the result to the resultList	
 		}
-				
+
 	}
-	
+
 	//TODO:Remove this method
-	private void printList(HashMap<String, List> listOfNodesAndRelations)
+	private void printList(List<List> listOfNodesAndRelations)
 	{
 		if(listOfNodesAndRelations != null)
 		{
-			List<IJsonNode> nodeList = listOfNodesAndRelations.get("nodesList");
-			if(nodeList != null)
-				for(IJsonNode jsonNode: nodeList)
+			for(List rowList: listOfNodesAndRelations)
+			{
+				System.out.println("--------------------------------------------");
+				for(Object obj: rowList)
 				{
-					System.out.println("Id: "+jsonNode.getId());
-					System.out.println("Type: "+jsonNode.getType());
-					System.out.println("Data size: "+jsonNode.getData().size());
-					Iterator<Entry<String, String>> iterator = jsonNode.getData().entrySet().iterator();
-					while(iterator.hasNext())
+					if(obj instanceof IJsonNode)
 					{
-						Map.Entry dataEntry = (Map.Entry) iterator.next();
-						System.out.println(dataEntry.getKey()+" : "+dataEntry.getValue());
+						IJsonNode jsonNode = (IJsonNode) obj;
+						System.out.println(">> Node <<");
+						System.out.println("Id: "+jsonNode.getId());
+						System.out.println("Type: "+jsonNode.getType());
+						System.out.println("Data size: "+jsonNode.getData().size());
+						Iterator<Entry<String, String>> iterator = jsonNode.getData().entrySet().iterator();
+						while(iterator.hasNext())
+						{
+							Map.Entry dataEntry = (Map.Entry) iterator.next();
+							System.out.println(dataEntry.getKey()+" : "+dataEntry.getValue());
+						}
+						System.out.println();
 					}
-					System.out.println();
-				}
-
-			List<IJsonRelation> relationList = listOfNodesAndRelations.get("relationList");
-			if(relationList != null)
-				for(IJsonRelation jsonRelation: relationList)
-				{
-					System.out.println("Id: "+jsonRelation.getId());
-					System.out.println("Start: "+jsonRelation.getStartNode());
-					System.out.println("End: "+jsonRelation.getEndNode());
-					System.out.println("Type: "+jsonRelation.getType());
-					System.out.println("Data size: "+jsonRelation.getData().size());
-					Iterator<Entry<String, String>> iterator = jsonRelation.getData().entrySet().iterator();
-					while(iterator.hasNext())
+					else if(obj instanceof IJsonRelation)
 					{
-						Map.Entry dataEntry = (Map.Entry) iterator.next();
-						System.out.println(dataEntry.getKey()+" : "+dataEntry.getValue());
+						IJsonRelation jsonRelation = (IJsonRelation) obj;
+						System.out.println(">> Relation <<");
+						System.out.println("Id: "+jsonRelation.getId());
+						System.out.println("Start: "+jsonRelation.getStartNode());
+						System.out.println("End: "+jsonRelation.getEndNode());
+						System.out.println("Type: "+jsonRelation.getType());
+						System.out.println("Data size: "+jsonRelation.getData().size());
+						Iterator<Entry<String, String>> iterator = jsonRelation.getData().entrySet().iterator();
+						while(iterator.hasNext())
+						{
+							Map.Entry dataEntry = (Map.Entry) iterator.next();
+							System.out.println(dataEntry.getKey()+" : "+dataEntry.getValue());
+						}
+						System.out.println();
 					}
-					System.out.println();
 				}
+				System.out.println("--------------------------------------------");
+			}
 		}
 	}
 }
