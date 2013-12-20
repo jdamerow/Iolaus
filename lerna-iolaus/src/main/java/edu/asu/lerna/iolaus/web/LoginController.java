b5b738cd01cc1c3382a33f849daab90fa6f20968
dlohith <dlohith@f4818639-45f7-4aff-89be-ea69564cad2b>
2013-12-20 18:28:44 +0000

    Adding User domain class

---
+++ lerna-iolaus/src/main/java/edu/asu/lerna/iolaus/web/LoginController.java
@@ -0,0 +1,16 @@
+package edu.asu.lerna.iolaus.web;
+
+import org.springframework.stereotype.Controller;
+
+
+/**
+ * The controller is to manage the login for user 
+ * 
+ * @author Lohith Dwaraka
+ */
+
+@Controller
+public class LoginController {
+
+	
+}
