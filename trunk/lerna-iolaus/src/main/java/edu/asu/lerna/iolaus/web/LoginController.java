b3c6773b6314a0ea99a9499c0980e70d82031c77
dlohith <dlohith@f4818639-45f7-4aff-89be-ea69564cad2b>
2013-12-20 21:48:35 +0000

    Login filter at spring security

--- trunk/lerna-iolaus/src/main/java/edu/asu/lerna/iolaus/web/LoginController.java
+++ trunk/lerna-iolaus/src/main/java/edu/asu/lerna/iolaus/web/LoginController.java
@@ -1,6 +1,9 @@
 package edu.asu.lerna.iolaus.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 /**
@@ -12,5 +15,30 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class LoginController {
 
-	
+	/**
+	 * User requests a login page
+	 * 
+	 * @return		Redirected to the login page
+	 */
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login(ModelMap model) {
+
+		return "login";
+
+	}
+ 
+	@RequestMapping(value="/loginfailed", method = RequestMethod.GET)
+	public String loginerror(ModelMap model) {
+ 
+		model.addAttribute("error", "true");
+		return "login";
+ 
+	}
+ 
+	@RequestMapping(value="/logout", method = RequestMethod.GET)
+	public String logout(ModelMap model) {
+ 
+		return "login";
+ 
+	}
 }
---
+++ trunk/lerna-iolaus/src/main/java/edu/asu/lerna/iolaus/web/RootController.java
@@ -0,0 +1,24 @@
+package edu.asu.lerna.iolaus.web;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * This class redirects requests to the root of Lerna to the Lerna home page.
+ * @author Lohith Dwaraka
+ *
+ */
+@Controller
+public class RootController {
+
+	/**
+	 * Method that answers requests to root.
+	 * @return the redirect path to the Quadriga home page.
+	 */
+	@RequestMapping(value="/", method = RequestMethod.GET)
+	public String redirectToLogin() {
+		return "redirect:auth/home";
+	}
+}
