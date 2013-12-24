2def08a9ab8d67cb7dbf2311c05ca8a7ad43343f
dlohith <dlohith@f4818639-45f7-4aff-89be-ea69564cad2b>
2013-12-24 21:10:10 +0000

    Adding converters

---
+++ trunk/lerna-iolaus/src/main/java/edu/asu/lerna/iolaus/converters/UserStringConverter.java
@@ -0,0 +1,19 @@
+package edu.asu.lerna.iolaus.converters;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+
+import edu.asu.lerna.iolaus.domain.implementation.User;
+import edu.asu.lerna.iolaus.service.IUserManager;
+
+public class UserStringConverter implements Converter<String, User>{
+	
+	@Autowired
+	private IUserManager manager;
+	
+	@Override
+	public User convert(String arg0) {
+		return manager.getUserById(arg0);
+	}
+
+}
