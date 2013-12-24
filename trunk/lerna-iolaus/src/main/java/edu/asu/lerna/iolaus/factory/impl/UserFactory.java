c856b8475a20940842ddaf4b242870f02036ac01
dlohith <dlohith@f4818639-45f7-4aff-89be-ea69564cad2b>
2013-12-24 00:41:21 +0000

    Updating pom and fixed a bug

--- trunk/lerna-iolaus/src/main/java/edu/asu/lerna/iolaus/factory/impl/UserFactory.java
+++ trunk/lerna-iolaus/src/main/java/edu/asu/lerna/iolaus/factory/impl/UserFactory.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.stereotype.Service;
 
 import edu.asu.lerna.iolaus.domain.implementation.Role;
 import edu.asu.lerna.iolaus.domain.implementation.User;
 import edu.asu.lerna.iolaus.factory.IUserFactory;
 import edu.asu.lerna.iolaus.service.login.LernaGrantedAuthority;
 
+@Service
 public class UserFactory implements IUserFactory {
 
 	@Override
