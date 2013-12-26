f4ee2c23757b46f3d86c434e8f8ebc5c9b390289
dlohith <dlohith@f4818639-45f7-4aff-89be-ea69564cad2b>
2013-12-26 23:52:11 +0000

    Adding own annotation and changes to jsp

---
+++ trunk/lerna-iolaus/src/main/java/edu/asu/lerna/iolaus/annotation/NotEmptyValidatorList.java
@@ -0,0 +1,32 @@
+package edu.asu.lerna.iolaus.annotation;
+
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class NotEmptyValidatorList implements ConstraintValidator<NotEmptyList, List<String>>{
+
+	@Override
+	public void initialize(NotEmptyList arg0) {
+
+	}
+
+	@Override
+	public boolean isValid(List<String> arg0, ConstraintValidatorContext arg1) {
+		if((arg0 != null)&&(arg0.size()!=0)){
+			return true;
+		}else if(arg0 == null){
+			return false;
+		}else{
+			for(String str : arg0){
+				if(str==null){
+					return false;
+				}
+			}
+		}
+		return false;
+	}
+
+
+}
