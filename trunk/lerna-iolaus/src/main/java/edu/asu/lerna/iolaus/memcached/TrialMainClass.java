888785389bda002fc6d8e5033f1c8a500bb58fe8
zambr0 <zambr0@f4818639-45f7-4aff-89be-ea69564cad2b>
2013-12-27 00:58:05 +0000

    Added test method for memcached

---
+++ trunk/lerna-iolaus/src/main/java/edu/asu/lerna/iolaus/memcached/TrialMainClass.java
@@ -0,0 +1,32 @@
+package edu.asu.lerna.iolaus.memcached;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+import net.spy.memcached.AddrUtil;
+import net.spy.memcached.MemcachedClient;
+import net.spy.memcached.internal.OperationFuture;
+
+public class TrialMainClass {
+
+	public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
+		
+		MemcachedClient memcachedClient = new MemcachedClient(AddrUtil.getAddresses("127.0.0.1:11211"));
+		
+		OperationFuture<Boolean> result = memcachedClient.set("1", 0, "One");
+		System.out.println(result.get());
+		result = memcachedClient.add("10", 0, "TEN");
+		System.out.println(result.get());
+		result = memcachedClient.set("2", 0, "Two");
+		System.out.println(result.get());
+		result = memcachedClient.set("3", 0, "Three");
+		System.out.println(result.get());
+		result = memcachedClient.set("4", 0, "Four");
+		System.out.println(result.get());
+		System.out.println("Inserted into cache");
+		System.out.println(memcachedClient.get("10"));
+		
+
+	}
+
+}
