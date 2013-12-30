b9e3326fd9fd41c2be46c57f861a169078299e74
dlohith <dlohith@f4818639-45f7-4aff-89be-ea69564cad2b>
2013-12-30 23:12:34 +0000

    Fixing validator

--- trunk/lerna-iolaus/src/main/java/edu/asu/lerna/iolaus/annotation/NotEmptyValidatorList.java
+++ trunk/lerna-iolaus/src/main/java/edu/asu/lerna/iolaus/annotation/NotEmptyValidatorList.java
@@ -5,7 +5,9 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
-public class NotEmptyValidatorList implements ConstraintValidator<NotEmptyList, List<String>>{
+import edu.asu.lerna.iolaus.domain.implementation.Role;
+
+public class NotEmptyValidatorList implements ConstraintValidator<NotEmptyList, List<Role>>{
 
 	@Override
 	public void initialize(NotEmptyList arg0) {
@@ -13,14 +15,14 @@ public class NotEmptyValidatorList implements ConstraintValidator<NotEmptyList, 
 	}
 
 	@Override
-	public boolean isValid(List<String> arg0, ConstraintValidatorContext arg1) {
+	public boolean isValid(List<Role> arg0, ConstraintValidatorContext arg1) {
 		if((arg0 != null)&&(arg0.size()!=0)){
 			return true;
 		}else if(arg0 == null){
 			return false;
 		}else{
-			for(String str : arg0){
-				if(str==null){
+			for(Role role : arg0){
+				if(role==null){
 					return false;
 				}
 			}
