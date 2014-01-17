69079b6ff55ee3777f9a9eca45e2025c32a84bcd
dlohith <dlohith@f4818639-45f7-4aff-89be-ea69564cad2b>
2014-01-17 18:55:09 +0000

    fixing the dependencies for Junit testcases

---
+++ trunk/lerna-iolaus/src/test/java/edu/asu/lerna/iolaus/factory/impl/UserFactoryTest.java
@@ -0,0 +1,69 @@
+package edu.asu.lerna.iolaus.factory.impl;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import edu.asu.lerna.iolaus.domain.implementation.Role;
+import edu.asu.lerna.iolaus.domain.implementation.User;
+import edu.asu.lerna.iolaus.factory.IUserFactory;
+import edu.asu.lerna.iolaus.service.IRoleManager;
+
+@ContextConfiguration(locations={
+"file:src/test/resources/root-context.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
+public class UserFactoryTest {
+
+	@Autowired
+	private IUserFactory userFactory;
+	
+	@Autowired
+	private IRoleManager roleManager;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCreateUserStringStringStringStringListOfRole() {
+		Role role = roleManager.getRole("ROLE_USER");
+		List<Role> roleList = new ArrayList<Role>();
+		roleList.add(role);
+		User user = userFactory.createUser("lernauser", "lerna", "lerna@iolaus.com", "test", roleList);
+		assertEquals(user.getEmail(),"lerna@iolaus.com");
+		assertEquals(user.getUsername(),"lernauser");
+		assertEquals(user.getName(), "lerna");
+		assertEquals(user.getAuthorities().get(0).getAuthority(), "ROLE_USER");
+		
+	}
+
+	@Test
+	public void testCreateUserStringStringStringStringRoleArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEncrypt() {
+		fail("Not yet implemented");
+	}
+
+}
