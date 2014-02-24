85caa05a35aa21ad1ac65f5b5f8b1acea90ba36a
karhko <karhko@f4818639-45f7-4aff-89be-ea69564cad2b>
2014-02-24 23:27:41 +0000

    changed Query controller

--- trunk/lerna-iolaus/src/main/java/edu/asu/lerna/iolaus/service/impl/XMLtoCypherConverter.java
+++ trunk/lerna-iolaus/src/main/java/edu/asu/lerna/iolaus/service/impl/XMLtoCypherConverter.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import edu.asu.lerna.iolaus.domain.queryobject.IDatabase;
 import org.springframework.stereotype.Service;
 
 import edu.asu.lerna.iolaus.domain.queryobject.IQuery;
