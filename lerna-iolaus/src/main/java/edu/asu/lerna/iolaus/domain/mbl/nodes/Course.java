1aa1850f266d8e78152d98c8c6813a0a4a209419
karhko <karhko@f4818639-45f7-4aff-89be-ea69564cad2b>
2014-08-29 02:41:18 +0000

    created domain classes for Nodes

---
+++ lerna-iolaus/src/main/java/edu/asu/lerna/iolaus/domain/mbl/nodes/Course.java
@@ -0,0 +1,52 @@
+package edu.asu.lerna.iolaus.domain.mbl.nodes;
+
+import java.io.IOException;
+
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+
+public class Course {
+	
+	private String type = "Course";
+	private String uri;
+	private String name;
+	private int year;
+	private String dataset = "mblcourses";
+	
+	public Course(String uri, String name, int year) {
+		this.uri = uri;
+		this.name = name;
+		this.year = year;
+	}
+
+	
+	public String getType() {
+		return type;
+	}
+	
+	public String getDataset() {
+		return dataset;
+	}
+
+	public String getUri() {
+		return uri;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public String toJson() throws JsonGenerationException, JsonMappingException, IOException{
+		ObjectMapper mapper = new ObjectMapper();
+		String json = mapper.writeValueAsString(this);
+		return json;
+	}
+
+}
+
+
---
+++ lerna-iolaus/src/main/java/edu/asu/lerna/iolaus/domain/mbl/nodes/Institution.java
@@ -0,0 +1,42 @@
+package edu.asu.lerna.iolaus.domain.mbl.nodes;
+
+import java.io.IOException;
+
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+
+public class Institution {
+	
+	private String type = "Institution";
+	private String uri;
+	private String name;
+	private String dataset = "mblcourses";
+	
+	public Institution(String uri, String name) {
+		this.uri = uri;
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+	
+	public String getDataset() {
+		return dataset;
+	}
+	
+	public String getUri() {
+		return uri;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String toJson() throws JsonGenerationException, JsonMappingException, IOException{
+		ObjectMapper mapper = new ObjectMapper();
+		String json = mapper.writeValueAsString(this);
+		return json;
+	}
+}
