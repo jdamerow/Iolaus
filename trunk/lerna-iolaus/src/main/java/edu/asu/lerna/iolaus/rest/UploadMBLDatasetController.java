b4ccfa95dd8bcfab13c164ce988692fb60ba0470
karhko <karhko@f4818639-45f7-4aff-89be-ea69564cad2b>
2014-09-05 01:25:40 +0000

    added validation, status codes  and messages

--- trunk/lerna-iolaus/src/main/java/edu/asu/lerna/iolaus/rest/UploadMBLDatasetController.java
+++ trunk/lerna-iolaus/src/main/java/edu/asu/lerna/iolaus/rest/UploadMBLDatasetController.java
@@ -3,6 +3,7 @@ package edu.asu.lerna.iolaus.rest;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.security.Principal;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,6 +12,9 @@ import javax.xml.bind.JAXBException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,9 +22,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import edu.asu.lerna.iolaus.configuration.neo4j.impl.Neo4jRegistry;
+import edu.asu.lerna.iolaus.exception.IndexPropertyException;
+import edu.asu.lerna.iolaus.exception.Neo4jInstanceIdDoesNotExist;
 import edu.asu.lerna.iolaus.service.INeo4jInstanceManager;
 import edu.asu.lerna.iolaus.service.IUploadMBLDataManager;
 
+@Controller
 public class UploadMBLDatasetController {
 	private static final Logger logger = LoggerFactory
 			.getLogger(UploadDatasetController.class);
@@ -46,119 +53,152 @@ public class UploadMBLDatasetController {
 	 * @return whether upload is successful or not.
 	 * @throws JAXBException
 	 * @throws IOException
+	 * @throws IndexPropertyException 
+	 * @throws Neo4jInstanceIdDoesNotExist 
 	 */
 	@ResponseBody
-	@RequestMapping(value = "/rest/data/mbl/upload/csv/affiliations", method = RequestMethod.POST)
+	@RequestMapping(value = "/rest/data/mbl/upload/{instanceId}/csv/affiliation", method = RequestMethod.POST)
 	public String uploadAffiliationDataset(HttpServletRequest request,
-			HttpServletResponse response,
-			@RequestParam("file") MultipartFile multipartfile)
-			throws JAXBException, IOException {
+			HttpServletResponse response, ModelMap model,Principal principal,
+			@RequestParam("file") MultipartFile multipartfile, @PathVariable("instanceId") String instanceId)
+			throws JAXBException, IOException, IndexPropertyException, Neo4jInstanceIdDoesNotExist {
 
-		logger.info("Uploading affiliation dataset");
 		File file = convertMultipartToFile(multipartfile);
-		boolean success = uploadManager.uploadHasAffilationDataset(file);
+		String fileName = file.getName();
 		String message;
 		int status;
-		if(success) {
-			message = "SUCCESS";
-			status = 200;
+		if((fileName.substring(fileName.lastIndexOf(".")).equalsIgnoreCase("csv"))) {
+			logger.info("Uploading affiliation dataset");
+			boolean success = uploadManager.uploadAffilationDataset(file, instanceId);
+			if(success) {
+				message = "SUCCESS";
+				status = 200;
+			} else {
+				message = "PARTIAL DATA UPLOADED";
+				status = 206;
+			}
+			logger.info("Finished Uploading affilation dataset");
 		} else {
-			message = "PARTIAL DATA UPLOADED";
-			status = 206;
+			status = 406;
+			message = "File format must be CSV";
 		}
-		logger.info("Finished Uploading affilation dataset");
+		
 		return jsonMessage(status, message);
 	}
 	
 	@ResponseBody
-	@RequestMapping(value = "/rest/data/mbl/upload/csv/attended", method = RequestMethod.POST)
+	@RequestMapping(value = "/rest/data/mbl/upload/{instanceId}/csv/attendance", method = RequestMethod.POST)
 	public String uploadAttendedDataset(HttpServletRequest request,
-			HttpServletResponse response,
-			@RequestParam("file") MultipartFile multipartfile)
-			throws JAXBException, IOException {
+			HttpServletResponse response, ModelMap model,Principal principal,
+			@RequestParam("file") MultipartFile multipartfile,  @PathVariable("instanceId") String instanceId)
+			throws JAXBException, IOException, IndexPropertyException, Neo4jInstanceIdDoesNotExist {
 
-		logger.info("Uploading Attended dataset");
 		File file = convertMultipartToFile(multipartfile);
-		boolean success = uploadManager.uploadAttendedDataset(file);
+		String fileName = file.getName();
 		String message;
 		int status;
-		if(success) {
-			message = "SUCCESS";
-			status = 200;
+		if((fileName.substring(fileName.lastIndexOf(".")).equalsIgnoreCase("csv"))) {
+			logger.info("Uploading Attended dataset");
+			boolean success = uploadManager.uploadAttendedDataset(file, instanceId);
+			if(success) {
+				message = "SUCCESS";
+				status = 200;
+			} else {
+				message = "PARTIAL DATA UPLOADED";
+				status = 206;
+			}
+			logger.info("Finished Uploading Attended dataset");
 		} else {
-			message = "PARTIAL DATA UPLOADED";
-			status = 206;
+			status = 406;
+			message = "File format must be CSV";
 		}
-		logger.info("Finished Uploading Attended dataset");
 		return jsonMessage(status, message);
 	}
 
 	@ResponseBody
-	@RequestMapping(value = "/rest/data/mbl/upload/csv/investigator", method = RequestMethod.POST)
+	@RequestMapping(value = "/rest/data/mbl/upload/{instanceId}/csv/investigator", method = RequestMethod.POST)
 	public String uploadIvestigatorDataset(HttpServletRequest request,
 			HttpServletResponse response,
-			@RequestParam("file") MultipartFile multipartfile)
-			throws JAXBException, IOException {
+			@RequestParam("file") MultipartFile multipartfile, @PathVariable("instanceId") String instanceId)
+			throws JAXBException, IOException, IndexPropertyException, Neo4jInstanceIdDoesNotExist {
 
-		logger.info("Uploading investigator dataset");
 		File file = convertMultipartToFile(multipartfile);
-		boolean success = uploadManager.uploadHasInvestigatorDataset(file);
+		String fileName = file.getName();
 		String message;
 		int status;
-		if(success) {
-			message = "SUCCESS";
-			status = 200;
+		if((fileName.substring(fileName.lastIndexOf(".")).equalsIgnoreCase("csv"))) {
+			logger.info("Uploading investigator dataset");
+			boolean success = uploadManager.uploadInvestigatorDataset(file, instanceId);
+			if(success) {
+				message = "SUCCESS";
+				status = 200;
+			} else {
+				message = "PARTIAL DATA UPLOADED";
+				status = 206;
+			}
+			logger.info("Finished Uploading investigator dataset");
 		} else {
-			message = "PARTIAL DATA UPLOADED";
-			status = 206;
+			status = 406;
+			message = "File format must be CSV";
 		}
-		logger.info("Finished Uploading investigator dataset");
 		return jsonMessage(status, message);
 	}
 	
 	@ResponseBody
-	@RequestMapping(value = "/rest/data/mbl/upload/csv/location", method = RequestMethod.POST)
+	@RequestMapping(value = "/rest/data/mbl/upload/{instanceId}/csv/location", method = RequestMethod.POST)
 	public String uploadLocationDataset(HttpServletRequest request,
 			HttpServletResponse response,
-			@RequestParam("file") MultipartFile multipartfile)
-			throws JAXBException, IOException {
+			@RequestParam("file") MultipartFile multipartfile, @PathVariable("instanceId") String instanceId)
+			throws JAXBException, IOException, IndexPropertyException, Neo4jInstanceIdDoesNotExist {
 
-		logger.info("Uploading location dataset");
 		File file = convertMultipartToFile(multipartfile);
-		boolean success = uploadManager.uploadHasLocationDataset(file);
+		String fileName = file.getName();
 		String message;
 		int status;
-		if(success) {
-			message = "SUCCESS";
-			status = 200;
+		if((fileName.substring(fileName.lastIndexOf(".")).equalsIgnoreCase("csv"))) {
+			logger.info("Uploading location dataset");
+			boolean success = uploadManager.uploadLocationDataset(file, instanceId);
+			if(success) {
+				message = "SUCCESS";
+				status = 200;
+			} else {
+				message = "PARTIAL DATA UPLOADED";
+				status = 206;
+			}
+			logger.info("Finished Uploading location dataset");
 		} else {
-			message = "PARTIAL DATA UPLOADED";
-			status = 206;
+			status = 406;
+			message = "File format must be CSV";
 		}
-		logger.info("Finished Uploading location dataset");
 		return jsonMessage(status, message);
 	}
 	
 	@ResponseBody
-	@RequestMapping(value = "/rest/data/mbl/upload/csv/partof", method = RequestMethod.POST)
+	@RequestMapping(value = "/rest/data/mbl/upload/{instanceId}/csv/coursegroup", method = RequestMethod.POST)
 	public String uploadIsPartOfDataset(HttpServletRequest request,
 			HttpServletResponse response,
-			@RequestParam("file") MultipartFile multipartfile)
-			throws JAXBException, IOException {
+			@RequestParam("file") MultipartFile multipartfile, @PathVariable("instanceId") String instanceId)
+			throws JAXBException, IOException, IndexPropertyException, Neo4jInstanceIdDoesNotExist {
 
-		logger.info("Uploading IsPartOf dataset");
 		File file = convertMultipartToFile(multipartfile);
-		boolean success = uploadManager.uploadIsPartOfDataset(file);
+		String fileName = file.getName();
 		String message;
 		int status;
-		if(success) {
-			message = "SUCCESS";
-			status = 200;
+		if((fileName.substring(fileName.lastIndexOf(".")).equalsIgnoreCase("csv"))) {
+			logger.info("Uploading CourseGroup dataset");
+			boolean success = uploadManager.uploadCourseGroupDataset(file, instanceId);
+			if(success) {
+				message = "SUCCESS";
+				status = 200;
+			} else {
+				message = "PARTIAL DATA UPLOADED";
+				status = 206;
+			}
+			logger.info("Finished Uploading IsPartOf dataset");
 		} else {
-			message = "PARTIAL DATA UPLOADED";
-			status = 206;
+			status = 406;
+			message = "File format must be CSV";
 		}
-		logger.info("Finished Uploading IsPartOf dataset");
 		return jsonMessage(status, message);
 	}
 	
