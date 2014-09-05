508bd3f8725240e2730a3bfad8e084057bde47ea
karhko <karhko@f4818639-45f7-4aff-89be-ea69564cad2b>
2014-09-05 01:11:28 +0000

    added getPropertyJson method in nodes

--- trunk/lerna-iolaus/src/main/java/edu/asu/lerna/iolaus/domain/mbl/nodes/Node.java
+++ trunk/lerna-iolaus/src/main/java/edu/asu/lerna/iolaus/domain/mbl/nodes/Node.java
@@ -1,6 +1,7 @@
 package edu.asu.lerna.iolaus.domain.mbl.nodes;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.map.JsonMappingException;
@@ -8,4 +9,25 @@ import org.codehaus.jackson.map.JsonMappingException;
 public abstract class Node {
 	public abstract String toJson() throws JsonGenerationException, JsonMappingException, IOException;
 	public abstract String getUri();
+	public abstract List<String> getPropertyJson(String nodeId);
+	
+	protected String createJson(String nodeId, String key, String value) {
+		StringBuilder json = new StringBuilder();
+		json.append("{");
+		json.append("\"key\" : \"" + key +"\",");
+		json.append("\"uri\" : \"" + nodeId + "\",");
+		json.append("\"value\" : \"" + value +"\"");
+		json.append("}");
+		return json.toString();
+	}
+	
+	protected String createJson(String nodeId, String key, int value) {
+		StringBuilder json = new StringBuilder();
+		json.append("{");
+		json.append("\"key\" : \"" + key +"\",");
+		json.append("\"uri\" : \"" + nodeId + "\",");
+		json.append("\"value\" : " + value);
+		json.append("}");
+		return json.toString();
+	}
 }
